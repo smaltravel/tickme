6f90ca539707be8e4e821aa55d93115ec86fc05d
Viacheslav <pvd.senior@yandex.ru>
2025-10-13 22:17:33 +0200

    Add TimeEntries table for Drift integration
    - Introduced TimeEntries table with columns for id, categoryId, startTime, and endTime.
    - Integrated TimeEntryModel with Drift using UseRowClass annotation.

--- lib/models/time_entry.dart
+++ lib/models/time_entry.dart
@@ -1,4 +1,6 @@
+import 'package:drift/drift.dart' as drift;
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:tickme/models/tick_category.dart';
 
 part 'generated/time_entry.freezed.dart';
 part 'generated/time_entry.g.dart';
@@ -14,4 +16,12 @@ abstract class TimeEntryModel with _$TimeEntryModel {
 
   factory TimeEntryModel.fromJson(Map<String, dynamic> json) =>
       _$TimeEntryModelFromJson(json);
+}
+
+@drift.UseRowClass(TimeEntryModel)
+class TimeEntries extends drift.Table {
+  drift.IntColumn get id => integer().autoIncrement()();
+  drift.IntColumn get categoryId => integer().references(TickCategories, #id)();
+  drift.DateTimeColumn get startTime => dateTime()();
+  drift.DateTimeColumn get endTime => dateTime()();
 }
